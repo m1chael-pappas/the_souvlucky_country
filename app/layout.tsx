c4cce9dc0a61995d6a70202ce564317ad22c8461
MichaelPappas2662 <mixalis.pappas23@gmail.com>
2025-07-12 21:34:17 +1000

    adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
-import './globals.css';
-import '@mantine/core/styles.css';
-import '@mantine/dates/styles.css';
+import "./globals.css";
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
 
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-import Footer from '@/components/layout/Footer';
-import Header from '@/components/layout/Header';
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 import {
   ColorSchemeScript,
   mantineHtmlProps,
   MantineProvider,
-} from '@mantine/core';
+} from "@mantine/core";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -158,7 +159,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <MantineProvider>
           <Header />
-          <main>{children}</main>
+          <main>
+            {children} <Analytics />
+          </main>
           <Footer />
         </MantineProvider>
       </body>
